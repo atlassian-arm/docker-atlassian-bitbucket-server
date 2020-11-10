c8d6a5eca4e8350af511bf1d7477a39aa64dd663
Steve Smith <ssmith@atlassian.com>
2020-11-10 11:30:27 +1100

    DCD-1131: Split startup into functions and add warning about running ES in the container.

--- entrypoint.py
+++ entrypoint.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3 -B
 
 from entrypoint_helpers import env, str2bool, start_app
+import logging
 
 
 RUN_USER = env['run_user']
@@ -8,8 +9,22 @@ RUN_GROUP = env['run_group']
 BITBUCKET_INSTALL_DIR = env['bitbucket_install_dir']
 BITBUCKET_HOME = env['bitbucket_home']
 
-start_cmd = f"{BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh -fg"
-if str2bool(env['elasticsearch_enabled']) is False or env['application_mode'] == 'mirror':
-    start_cmd += ' --no-search'
 
-start_app(start_cmd, BITBUCKET_HOME, name='Bitbucket Server')
+def start_full():
+    logging.warning("Starting container with local ElasticSearch. "
+                 "This is not recommended, and may cause issues with clean shutdown. "
+                 "It is recommended to run a separate ElasticSearch container, and set 'elasticsearch_enabled' to false.")
+    start_cmd = f"{BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh -fg"
+    start_app(start_cmd, BITBUCKET_HOME, name='Bitbucket Server')
+
+
+def start_bb_only():
+    # Interim
+    start_cmd = f"{BITBUCKET_INSTALL_DIR}/bin/start-bitbucket.sh -fg --no-search"
+    start_app(start_cmd, BITBUCKET_HOME, name='Bitbucket Server')
+
+
+if str2bool(env['elasticsearch_enabled']) is False or env['application_mode'] == 'mirror':
+    start_full()
+else:
+    start_bb_only()
