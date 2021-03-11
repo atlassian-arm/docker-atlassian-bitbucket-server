4060b4219f1490a7a50664c85ddd736e7d482087
Adam Brokes <abrokes@atlassian.com>
2021-03-11 16:33:10 +1100

    DCKUBE-143: Move testdata to fixtures

--- func-tests/smoketests/tests/test_bitbucket.py
+++ func-tests/smoketests/tests/test_bitbucket.py
@@ -1,51 +1,33 @@
 import requests
-import json
-import os
-import time
-
-from requests.models import HTTPBasicAuth
-
-BASE_REST_URL = os.environ.get('BITBUCKET_BASE_URL', "http://localhost:7990")
-user = os.environ.get('BITBUCKET_ADMIN', 'admin')
-password = os.environ.get('BITBUCKET_ADMIN_PWD', 'admin')
-auth = HTTPBasicAuth(user, password)
-
-project_key = f"PROJECT{round(time.time())}"
-project_name= f"My Project {round(time.time())}"
-repository_name = f"avatar{round(time.time())}"
 
 
-def test_create_project():
-    url = f"{BASE_REST_URL}/rest/api/1.0/projects"
+def test_create_project(ctx, tdata):
+    url = f"{ctx.base_url}/rest/api/1.0/projects"
 
     data = {
-        "key": project_key,
-        "name": project_name,
+        "key": tdata.project_key,
+        "name": tdata.project_name,
         "description": "The description for my cool project.",
     }
-    headers = {'Content-Type': 'application/json'}
 
-    r = requests.post(url, json=data, headers=headers, auth=auth)
-    data = r.json()
+    r = requests.post(url, json=data, auth=ctx.admin_auth)
 
     assert r.status_code == 201, f'failed to create project, status:{r.status_code}, content: {r.text}'
-    assert data['key'] == project_key
+    assert r.json()['key'] == tdata.project_key
 
 
-def test_create_repository():
-    url = f"{BASE_REST_URL}/rest/api/1.0/projects/{project_key}/repos"
+def test_create_repository(ctx, tdata):
+    url = f"{ctx.base_url}/rest/api/1.0/projects/{tdata.project_key}/repos"
 
     data = {
-        "name": repository_name,
+        "name": tdata.repository_name,
         "scmId": "git",
         "forkable": True,
         "defaultBranch": "main"
     }
-    headers = {'Content-Type': 'application/json'}
 
-    r = requests.post(url, json=data, headers=headers, auth=auth)
-    data = r.json()
+    r = requests.post(url, json=data, auth=ctx.admin_auth)
 
-    assert r.status_code == 201, f'failed to create repository, status: s{r.status_code}, content: {r.text}'
-    assert data['name'] == repository_name
-    assert data['slug'] == repository_name.lower()
+    assert r.status_code == 201, f'failed to create repository, status: {r.status_code}, content: {r.text}'
+    assert r.json()['name'] == tdata.repository_name
+    assert r.json()['slug'] == tdata.repository_name.lower()
