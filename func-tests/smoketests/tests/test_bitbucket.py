c1b589318fe347896f9043787e69b5c26b2e62aa
Adam Brokes <abrokes@atlassian.com>
2021-03-15 15:13:39 +1100

    DCKUBE-144: Import repository test case
    - add clone and push for a test resitory from a public github repository
    - increase test verbosity
    - add --cleanup option to delete the project and repository after tests

--- func-tests/smoketests/tests/test_bitbucket.py
+++ func-tests/smoketests/tests/test_bitbucket.py
@@ -1,4 +1,9 @@
+import logging
+from urllib.parse import urlparse
+
 import requests
+import subprocess
+import pytest
 
 
 def test_create_project(ctx, tdata):
@@ -31,3 +36,24 @@ def test_create_repository(ctx, tdata):
     assert r.status_code == 201, f'failed to create repository, status: {r.status_code}, content: {r.text}'
     assert r.json()['name'] == tdata.repository_name
     assert r.json()['slug'] == tdata.repository_name.lower()
+
+
+def test_import_repository(ctx, tdata):
+    clone_o = subprocess.run(
+        ["git", "clone", tdata.repo_to_clone, tdata.folder])
+    if clone_o.returncode not in (0, 128):
+        pytest.fail(f"error when cloning repository, {clone_o}")
+
+    url_parsed = urlparse(ctx.base_url)
+    # example scm url:
+    # http://admin:admin@localhost:7990/scm/project1615521268/avatar1615521268.git
+    repo_host_url = f"{url_parsed.scheme}://{ctx.admin_user}:{ctx.admin_pwd}@{url_parsed.hostname}:{url_parsed.port}" \
+                    f"/scm/{tdata.project_key.lower()}/{tdata.repository_name}.git"
+
+    clone_o = subprocess.run(["git", "remote", "add", tdata.project_key, repo_host_url], cwd=tdata.folder)
+
+    logging.info("Git clone returned with exit code: %d", clone_o.returncode)
+
+    push_o = subprocess.run(["git", "push", "-u", tdata.project_key, "--all"], cwd=tdata.folder)
+
+    assert push_o.returncode == 0, "cannot push repository from local to bitbucket"
