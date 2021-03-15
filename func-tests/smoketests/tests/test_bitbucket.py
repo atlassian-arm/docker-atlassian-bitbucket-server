20c32786fa21f31e4aa8321a2206374d86deb7c8
Adam Brokes <abrokes@atlassian.com>
2021-03-15 16:17:25 +1100

    DCKUBE-144: Use bare repository clone and mirror it

--- func-tests/smoketests/tests/test_bitbucket.py
+++ func-tests/smoketests/tests/test_bitbucket.py
@@ -40,7 +40,7 @@ def test_create_repository(ctx, tdata):
 
 def test_import_repository(ctx, tdata):
     clone_o = subprocess.run(
-        ["git", "clone", tdata.repo_to_clone, tdata.folder])
+        ["git", "clone", "--bare", tdata.repo_to_clone, tdata.folder])
     if clone_o.returncode not in (0, 128):
         pytest.fail(f"error when cloning repository, {clone_o}")
 
@@ -50,10 +50,12 @@ def test_import_repository(ctx, tdata):
     repo_host_url = f"{url_parsed.scheme}://{ctx.admin_user}:{ctx.admin_pwd}@{url_parsed.hostname}:{url_parsed.port}" \
                     f"/scm/{tdata.project_key.lower()}/{tdata.repository_name}.git"
 
-    clone_o = subprocess.run(["git", "remote", "add", tdata.project_key, repo_host_url], cwd=tdata.folder)
+    clone_o = subprocess.run(
+        ["git", "remote", "add", tdata.project_key, repo_host_url], cwd=tdata.folder)
 
     logging.info("Git clone returned with exit code: %d", clone_o.returncode)
 
-    push_o = subprocess.run(["git", "push", "-u", tdata.project_key, "--all"], cwd=tdata.folder)
+    push_o = subprocess.run(
+        ["git", "push", "--mirror", tdata.project_key], cwd=tdata.folder)
 
     assert push_o.returncode == 0, "cannot push repository from local to bitbucket"
